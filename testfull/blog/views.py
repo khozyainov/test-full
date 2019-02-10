9f13b341aaec8e7932cfef5c9743c89d013dbf2b
Хозяинов Антон <anton.h@nekidaem.ru>
2019-02-10 15:31:47 +0300

    add rest api

---
+++ testfull/blog/views.py
@@ -0,0 +1,63 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Blog
+from .serializers import BlogSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+@permission_classes((IsAuthenticated, IsOwnerOrReadOnly,))
+def get_delete_update_blog(request, pk):
+    try:
+        blog = Blog.objects.get(pk=pk)
+    except Blog.DoesNotExist:
+        content = {
+            'status': 'Not Found'
+        }
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = BlogSerializer(blog)
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if request.user == blog.author:
+            blog.delete()
+            content = {
+                'status': 'NO CONTENT'
+            }
+            return Response(content, status=status.HTTP_204_NO_CONTENT)
+        else:
+            content = {
+                'status': 'UNAUTHORIZED'
+            }
+            return Response(content, status=status.HTTP_401_UNAUTHORIZED)
+    elif request.method == 'PUT':
+        if request.user == blog.author:
+            serializer = BlogSerializer(blog, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            content = {
+                'status': 'UNAUTHORIZED'
+            }
+            return Response(content, status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['GET', 'POST'])
+@permission_classes((IsAuthenticated, ))
+def get_post_blogs(request):
+    if request.method == 'GET':
+        blogs = Blog.objects.all()
+        serializer = BlogSerializer(blogs, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(creator=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
